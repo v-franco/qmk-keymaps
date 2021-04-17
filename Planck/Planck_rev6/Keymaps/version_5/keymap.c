5150af61a0af3ec5d914e3ce87c91098dfc9fdc7
Victor Franco <vfranco.itc@gmail.com>
2021-04-17 11:09:47 -0500

    Finished keymap.c
    Added in comment lines the layout for each layer

--- Planck/Planck_rev6/Keymaps/version_5/keymap.c
+++ Planck/Planck_rev6/Keymaps/version_5/keymap.c
@@ -29,8 +29,12 @@ Hold for Layer 4 - |Tab  | A   | S   | D   | F   | G   | H   | J   | K   | L   |
                    |-----------------------------------------------------------------------|
   Tap for [ ------ |Ctrl |GUI  | {   |Alt  | Fn1 |Space|Bksp | Fn2 |RAlt | }   | -   |Ctrl | -- Tap for ]
                    `-----------------------------------------------------------------------'
-                                                         /
-  Tap for Shift -----------------------------------------
+                                              /          /      /
+  Tap for Shift -----------------------------------------      /
+                                            /                 /
+   Hold for num layer ----------------------                 /
+                                                            /
+   Hold for symbol layer -----------------------------------
 
  */     
 
@@ -51,8 +55,12 @@ Hold for Layer 4 - |Tab  | A   | R   | S   | T   | D   | H   | N   | E   | I   |
                    |-----------------------------------------------------------------------|
   Tap for [ ------ |Ctrl |GUI  | {   |Alt  | Fn1 |Space|Bksp | Fn2 |RAlt | }   | -   |Ctrl | -- Tap for ]
                    `-----------------------------------------------------------------------'
-                                                         /
-  Tap for Shift -----------------------------------------
+                                              /          /      /
+  Tap for Shift -----------------------------------------      /
+                                            /                 /
+   Hold for num layer ----------------------                 /
+                                                            /
+   Hold for symbol layer -----------------------------------
 
  */     
 
@@ -63,12 +71,38 @@ Hold for Layer 4 - |Tab  | A   | R   | S   | T   | D   | H   | N   | E   | I   |
             LCTL_T(KC_LBRC), KC_LGUI, KC_LCBR, KC_LALT, TT(2), KC_SPC, LSFT_T(KC_BSPC), TO(3),   KC_RALT, KC_RCBR, KC_MINS, RCTL_T(KC_RBRC)
             ),
 
+/* numeric layer
+                   ,-----------------------------------------------------------------------.
+                   | Esc | 1   | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   | 0   |Enter| 
+                   |-----------------------------------------------------------------------|
+Hold for Layer 4 - |Tab  | F1  | F2  | F3  | F4  | F5  | F6  | 4   | 5   | 6   |  +  |  -  | 
+                   |-----------------------------------------------------------------------|
+  Tap for ( ------ |Shift| F7  | F8  | F9  | F10 | F11 | F12 | 1   | 2   | 3   |  *  |  /  | 
+                   |-----------------------------------------------------------------------|
+  Tap for [ ------ |Ctrl |GUI  | {   |Alt  | Fn1 |Space|Bksp |  .  | 0   | =   |  (  |  )  | 
+                   `-----------------------------------------------------------------------'
+
+ */     
+
 [_LAYER2] = LAYOUT(
             KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,   KC_8, KC_9,   KC_0,    KC_ENT,
             KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_4,   KC_5, KC_6,   KC_PLUS, KC_MINS,
             KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  KC_1,   KC_2, KC_3,   KC_ASTR, KC_SLSH,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_BSPC, KC_DOT, KC_0, KC_EQL, KC_LPRN, KC_RPRN
             ),
+
+/* symbol layer
+                   ,-----------------------------------------------------------------------.
+                   |  ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  (  |  )  |  =  |     | 
+                   |-----------------------------------------------------------------------|
+Hold for Layer 4 - |Tab  |  <  |  >  |  -  |  _  |     |     |  *  |  {  |  }  |  \  |     | 
+                   |-----------------------------------------------------------------------|
+  Tap for ( ------ |Shift|     |     |     |     |     |     |     |  [  |  ]  |  |  |     | 
+                   |-----------------------------------------------------------------------|
+  Tap for [ ------ |Ctrl |GUI  | {   |Alt  |     |Space|Bksp | Fn2 |     |     |     |     | 
+                   `-----------------------------------------------------------------------'
+
+ */     
 
 [_LAYER3] = LAYOUT(
             KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_EQL,  KC_NO,
@@ -77,12 +111,41 @@ Hold for Layer 4 - |Tab  | A   | R   | S   | T   | D   | H   | N   | E   | I   |
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_SPC,  KC_BSPC, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
             ),
 
+/* navigation layer
+                   ,-----------------------------------------------------------------------.
+                   | Esc | Cps | Up  |Vol- |Vol+ |Mute |Pg Up|Pg Dn|Home | End | Del |     | 
+                   |-----------------------------------------------------------------------|
+                   |     |Left |Down |Right|Save |Calc |Left |Down | Up  |Right| Ins |     | 
+                   |-----------------------------------------------------------------------|
+  Tap for ( ------ |Shift|     | Cut |Copy |Paste|     |     |     | Br- | Br+ |     |     | 
+                   |-----------------------------------------------------------------------|
+  Tap for [ ------ |Ctrl |GUI  |     |Alt  |     |Space|Bksp |     |RAlt |Menu |     | L5  | ----- Hold for layer settings layer
+                   `-----------------------------------------------------------------------'
+
+ */          
+
 [_LAYER4] = LAYOUT(
             KC_TRNS, KC_CAPS, KC_UP,      KC_VOLD,    KC_VOLU,    KC_MUTE, KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL, KC_NO,
             KC_TRNS, KC_LEFT, KC_DOWN,    KC_RGHT,    LCTL(KC_S), KC_CALC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS, KC_NO, 
             KC_TRNS, KC_NO,   LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,   KC_NO,   KC_NO,   KC_BRID, KC_BRIU, KC_NO,  KC_NO,
             KC_TRNS, KC_TRNS, KC_NO,      KC_TRNS,    KC_NO,      KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, KC_APP,  KC_NO, MO(5)
             ),
+
+/* settings layer
+
+Debug mode-----------------------------------------------------------------------
+                                                                                 \
+                   ,-----------------------------------------------------------------------.
+                   |BlTog|Bl on|BlOff|     |     |     |     |     |     |     |DEBUG|RESET| ----- Reset board to load new firmware
+                   |-----------------------------------------------------------------------|
+                   |     |     |     |     |     |     |     |     |     |     | L0  | L1  | ----- Set default layer QWERTY (0) or COLEMAK (1)
+                   |-----------------------------------------------------------------------|
+                   |     |     |     |     |     |     |     |     |     |     |     |     | 
+                   |-----------------------------------------------------------------------|
+                   |     |     |     |     |     |     |     |     |     |     |     | L5  | ----- Let go to exit settings layer 
+                   `-----------------------------------------------------------------------'
+
+ */         
 
 [_LAYER5] = LAYOUT(
             BL_TOGG, BL_ON, BL_OFF, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DEBUG, RESET, 
